0a1a9325136b574daadff955436ee8fa5d4a8009
Laoode <laodemuhammadyudhyprayitno@gmail.com>
2026-08-24 15:12:27 +0800

    chore: remove obsolete repository artifacts

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-echo -e "${GREEN}Installing all dependencies (workspace)...${NC}"
-uv sync
-echo -e "${GREEN}All dependencies installed.${NC}"
